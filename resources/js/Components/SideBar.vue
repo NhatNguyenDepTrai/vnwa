996ab59f49e9d2b98e230b4b30de0d54575c31ed
NhatNguyenDepTrai <nhatnguyen061003@gmail.com>
2023-11-29 23:06:38 +0700

    CodeSideBar2

---
+++ resources/js/Components/SideBar.vue
@@ -0,0 +1,48 @@
+<template>
+    <aside id="sidebar" class="w-full p-1  dark:bg-gray-900 dark:text-gray-100 h-screen">
+        <div class="sidebar-header">
+            <div class="logo">
+                <img src="/images/vnwaLogoFull.png" class="show" alt="Vinawebapp Logo">
+                <img src="/images/vnwaLogoIcon.png" class="hide" alt="Vinawebapp Logo">
+            </div>
+            <div class="btn">
+
+                <span className='relative inline-block overflow-hidden rounded-full p-[1px]'>
+                    <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]' />
+                    <div className='inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950/90 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl'>
+                        <icon icon="fa-xmark" class="show" />
+                        <icon icon="fa-bars" class="hide" />
+                    </div>
+                </span>
+            </div>
+        </div>
+        <div class="sidebar-content">
+            <ul>
+                <li class="mb-3">
+                    <SideBarLink :href="route('dashboard')" :active="route().current('dashboard')">
+                        <icon icon="fa-home" class="mr-2" />
+                        <span>Trang chủ</span>
+                    </SideBarLink>
+                </li>
+                <li class="mb-3">
+                    <SideBarLink :href="route('dashboard')" :active="route().current('setting')">
+                        <icon icon="fa-home" class="mr-2" />
+                        <span>Trang chủ</span>
+                    </SideBarLink>
+                </li>
+
+            </ul>
+        </div>
+    </aside>
+</template>
+<script>
+import SideBarLink from '@/Components/SideBarLink.vue';
+
+export default {
+    name: 'SideBar',
+    components: {
+        SideBarLink,
+    },
+
+};
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,27 @@
 import './bootstrap';
 import '../css/app.css';
+import '../css/style.css';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
-
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fas)
+library.add(fab)
+const appName = import.meta.env.VITE_APP_NAME || 'VNWA ADMIN';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title} - VNWA ADMIN`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .component('icon', FontAwesomeIcon)
+
             .mount(el);
     },
     progress: {
